f489cdbe0d415c300db0c7b7a1d9731e951f7ddc
Alexander Beal <abeal@twitter.com>
2015-12-05 16:18:05 -0700

    Chance license. Minor refactoring.

--- src/Main.hs
+++ src/Main.hs
@@ -25,7 +25,7 @@ main = do
     readUntilPrompt s B.empty >>= B.putStr
     scriptContents <- B.readFile scriptPath
     writeToFile s scriptName scriptContents
-    when (configExecAfter config) (sendReadPrint s ("dofile(\"" `B.append` scriptName `B.append` "\")" `B.append` endOfCommand))
+    when (configExecAfter config) (sendReadPrint s (doFileCommand scriptName))
     closeSerial s
     putStrLn ""
 
@@ -33,28 +33,23 @@ main = do
 -- ready for commands.
 writeToFile :: SerialPort -> B.ByteString -> B.ByteString -> IO ()
 writeToFile s name contents = do
-    sendReadPrint s openCommand
-    _ <- traverse (sendReadPrint s . writeLine) (B.lines contents)
+    sendReadPrint s (openCommand name)
+    _ <- traverse (sendReadPrint s . writeLineCommand) (B.lines contents)
     sendReadPrint s closeCommand
     return ()
 
-    where 
-        openCommand  = "file.open(\"" `B.append` name `B.append` "\", \"w+\")" `B.append` endOfCommand
-        writeLine ln = "file.writeline([[" `B.append` ln `B.append` "]])" `B.append` endOfCommand
-        closeCommand = "file.close()" `B.append` endOfCommand
-
--- Send a command and print the read response.
+-- Send a command and print the response.
 sendReadPrint :: SerialPort -> B.ByteString -> IO ()
 sendReadPrint s bs = sendRead s bs >>= B.putStr
 
--- Send a command and return the read response.
+-- Send a command and return the response.
 sendRead :: SerialPort -> B.ByteString -> IO B.ByteString
 sendRead s bs = do
     send_ s bs
     readUntilPrompt s B.empty
 
 -- Continue reading from the serial port until we get a prompt.
--- Return the read strings.
+-- Return what was read in.
 readUntilPrompt :: SerialPort -> B.ByteString -> IO B.ByteString
 readUntilPrompt s acc = do
     output <- recv s 128
@@ -75,6 +70,18 @@ containsPrompt bs =
 send_ :: SerialPort -> B.ByteString -> IO ()
 send_ s bs = send s bs *> pure ()
 
+writeLineCommand :: B.ByteString -> B.ByteString
+writeLineCommand ln = "file.writeline([[" `B.append` ln `B.append` "]])" `B.append` endOfCommand
+
+closeCommand :: B.ByteString
+closeCommand = "file.close()" `B.append` endOfCommand
+
+openCommand :: B.ByteString -> B.ByteString
+openCommand fileName = "file.open(\"" `B.append` fileName `B.append` "\", \"w+\")" `B.append` endOfCommand
+
+doFileCommand :: B.ByteString -> B.ByteString
+doFileCommand fileName = "dofile(\"" `B.append` fileName `B.append` "\")" `B.append` endOfCommand
+
 -- Character sequence for ending a command.
 endOfCommand :: B.ByteString
 endOfCommand = "\r\n"
@@ -86,7 +93,7 @@ espSettings = defaultSerialSettings
 opts :: OP.ParserInfo Config
 opts = OP.info (OP.helper <*> argumentParser)
      (  OP.fullDesc
-     <> OP.progDesc "Uploads a file to a HUZZAH board."
+     <> OP.progDesc "Uploads a file to an esp8266 board running nodemcu."
      )
 
 argumentParser :: OP.Parser Config
@@ -107,7 +114,7 @@ argumentParser = Config
             (  OP.short 'n'
             <> OP.long "name"
             <> OP.metavar "NAME"
-            <> OP.help "Name once uploaded to board."
+            <> OP.help "Name on board."
             )
     <*> OP.switch
             (  OP.long "exec"
